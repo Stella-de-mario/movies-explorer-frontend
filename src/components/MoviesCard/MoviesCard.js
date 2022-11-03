ad745d7bcd1e25212643a02cb01d2bc03834c595
Stella-de-mario <irina17otr@gmail.com>
2022-11-03 13:04:55 +0500

    v.3

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -6,14 +6,22 @@ import initialMovies from '../../utils/initialMovies';
 function MoviesCard(initialMovies) {
   const { pathname } = useLocation();
 
-  const [favorite, setFavorite] = React.useState(false);
+  const [isSave, setIsSave] = React.useState(false);
 
-  function handleFavoriteBtnClick() {
-    setFavorite(!favorite);
+  function handleSaveBtnClick() {
+    setIsSave(!isSave);
   }
 
   return (
     <div className="movie-card">
+      <a className='movie-card__link' href={initialMovies.trailerLink} target={"_blank"}
+            rel="noreferrer">
+      <img 
+        src={initialMovies.image}
+        alt={initialMovies.nameRU} 
+        className="movie-card__image"
+     />
+     </a>
       <div className="movie-card__container">
         <div className="movie-card__info">
           <h2 className="movie-card__name">
@@ -27,24 +35,19 @@ function MoviesCard(initialMovies) {
         {pathname === '/saved-movies'
 
         ? <button 
-            className="movie-card__btn movie-card__btn_delete"
+            className="movie-card__button movie-card__button_delete"
             type="button"
           />
 
         : <button
-            className={`movie-card__btn ${
-              favorite ? "movie-card__btn_favorites-active" : "movie-card__btn"}`}
+            className={`movie-card__button ${
+              isSave ? "movie-card__button_save-active" : "movie-card__button"}`}
             type="button"
-            onClick={handleFavoriteBtnClick}
+            onClick={handleSaveBtnClick}
           />}
           
       </div>
-      <img 
-        src={initialMovies.image}
-        alt={initialMovies.nameRU} 
-        className="movie-card__image"
-      >
-      </img>
+      
     </div>
   )
 }
