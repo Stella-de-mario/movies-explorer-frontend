301af9e236eb0e61ef1e81792f8e8d05bcc21f5b
Stella-de-mario <irina17otr@gmail.com>
2022-11-03 14:13:53 +0500

    v.3

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "./MoviesCard.css";
-import initialMovies from '../../utils/initialMovies';
+import { initialMovies } from "../../utils/initialMovies.js";
 
-function MoviesCard(initialMovies) {
+function MoviesCard() {
   const { pathname } = useLocation();
 
   const [isSave, setIsSave] = React.useState(false);
@@ -14,42 +14,37 @@ function MoviesCard(initialMovies) {
 
   return (
     <div className="movie-card">
-      <a className='movie-card__link' href={initialMovies.trailerLink} target={"_blank"}
-            rel="noreferrer">
-      <img 
+      <img
         src={initialMovies.image}
-        alt={initialMovies.nameRU} 
+        alt={initialMovies.nameRU}
         className="movie-card__image"
-     />
-     </a>
+      />
+
       <div className="movie-card__container">
         <div className="movie-card__info">
           <h2 className="movie-card__name">
             {initialMovies.nameRU}33 слова о дизайне
           </h2>
-          <p className="movie-card__duration">
-            {initialMovies.duration}1ч 47м
-          </p>
+          <p className="movie-card__duration">{initialMovies.duration}1ч 47м</p>
         </div>
 
-        {pathname === '/saved-movies'
-
-        ? <button 
+        {pathname === "/saved-movies" ? (
+          <button
             className="movie-card__button movie-card__button_delete"
             type="button"
           />
-
-        : <button
+        ) : (
+          <button
             className={`movie-card__button ${
-              isSave ? "movie-card__button_save-active" : "movie-card__button"}`}
+              isSave ? "movie-card__button_save-active" : "movie-card__button"
+            }`}
             type="button"
             onClick={handleSaveBtnClick}
-          />}
-          
+          />
+        )}
       </div>
-      
     </div>
-  )
+  );
 }
 
 export default MoviesCard;
