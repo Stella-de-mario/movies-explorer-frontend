fee7ac9ab1ec0331ddb9ea4b89c8de46cf1abe20
Stella-de-mario <irina17otr@gmail.com>
2022-11-05 23:03:13 +0500

    v.5

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -3,13 +3,14 @@ import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({ movies }) {
-  const cardsElement = movies.map((movie) => (
-    <MoviesCard key={movie._id} movieCard={movie} />
-  ));
   
   return (
     <section className="movies-cards">
-      <ul className="movies-cards__container">{cardsElement}</ul>
+      <ul className="movies-cards__list">{movies.map((card) => (
+        <li key={card._id}>
+          <MoviesCard card={card} />
+        </li>
+      ))}</ul>
       <div className="movies-cards__button-container">
         <button className="movies-cards__button" type="button">
           Ещё
